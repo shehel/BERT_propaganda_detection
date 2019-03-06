2984270325acf6758ee6c3a4572aa5f128fe9021
shehel <shehel@live.com>
2019-03-07 00:58:15 +0300

    opt step

--- train.py
+++ train.py
@@ -99,7 +99,7 @@ def main():
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
-    
+    # t_total matters
     optimizer = BertAdam(optimizer_grouped_parameters,
                          lr=opt.LR,
                          warmup=warmup_proportion,
