a5c0a105cebe51bde6332a8ac18ab71b12e68aa6
Seunghak Yu <seunghak.yu@gmail.com>
2019-03-20 17:46:38 +0000

    Implemented pad_sequences method to eliminate unnecessary dependency on keras(tensorflow)

--- tokenize_text.py
+++ tokenize_text.py
@@ -2,10 +2,32 @@ import pandas as pd
 import ast
 from opt import opt
 import numpy as np
-from keras.preprocessing.sequence import pad_sequences
+#from keras.preprocessing.sequence import pad_sequences
+import torch
 
 hash_token = 19
 end_token = 20
+
+def pad_sequences(sequences, batch_first=True, padding_value=0.0, max_len=0):
+    tmp = torch.Tensor(sequences[0])
+    max_size = tmp.size()
+    trailing_dims = max_size[1:]
+    
+    if batch_first:
+        out_dims = (len(sequences), max_len) + trailing_dims
+    else:
+        out_dims = (max_len, len(sequences)) + trailing_dims
+
+    out_tensor = tmp.data.new(*out_dims).fill_(padding_value)
+    for i, list in enumerate(sequences):
+        tensor = torch.Tensor(list)
+        length = tensor.size(0)
+        if batch_first:
+            out_tensor[i, :length, ...] = tensor
+        else:
+            out_tensor[:length, i, ...] = tensor
+
+    return out_tensor.long().numpy()
 
 def set_global_label(bio=False):
     global hash_token
@@ -79,13 +101,15 @@ def make_set(data_dir, tokenizer, class_type, bio = False):
     else:
         label_l = reg_encoding(cleaned, labels)
 
-
     input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
-                          maxlen=opt.maxLen, dtype="long", truncating="post", padding="post")
+                          padding_value=0.0, max_len=opt.maxLen)
+    #input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
+    #                      maxlen=opt.maxLen, dtype="long", truncating="post", padding="post")
     
-    tags = pad_sequences(label_l,
-                     maxlen=opt.maxLen, value=end_token, padding="post",
-                     dtype="long", truncating="post")
+    tags = pad_sequences(label_l, padding_value=end_token, max_len=opt.maxLen)
+    #tags = pad_sequences(label_l,
+    #                 maxlen=opt.maxLen, value=end_token, padding="post",
+    #                 dtype="long", truncating="post")
     attention_masks = [[float(i>0) for i in ii] for ii in input_ids]
     
     
